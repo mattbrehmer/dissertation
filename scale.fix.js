a12effa88eccd87e2d00a497460ee0fa5c0a29f3
Matt Brehmer <mattbrehmer@gmail.com>
2017-06-01 11:09:09 -0700

    Create scale.fix.js

---
+++ scale.fix.js
@@ -0,0 +1,25 @@
+var metas = document.getElementsByTagName('meta');
+var i;
+if (navigator.userAgent.match(/iPhone/i)) {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+    }
+  }
+  document.addEventListener("gesturestart", gestureStart, false);
+}
+function gestureStart() {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    }
+  }
+}
+/*
+     FILE ARCHIVED ON 17:14:06 Dec 26, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:07:00 Jun 01, 2017.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
